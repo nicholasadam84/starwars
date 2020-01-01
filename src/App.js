b6c2794451da9cf799fc9922f751be1704d879b5
Looney-Solutions <thelooneysolutions@gmail.com>
2019-12-31 18:54:52 -0800

    initial commit - ships are fetched and state is set correctly

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
+class App extends Component {
+  constructor() {
+      super()
+      this.state = {
+          starships: []
+      }
+  }  
+  componentDidMount() {
+    const starshipsArray = [];
+    //TO-DO: Iterate through these URLs until data.next !== null (the next: property is not null)
+    const urls = [
+        'https://swapi.co/api/starships/?page=2',
+        'https://swapi.co/api/starships/?page=1',
+        'https://swapi.co/api/starships/?page=3',
+        'https://swapi.co/api/starships/?page=4',
+    ];
+    Promise.all(urls.map(url => {
+        return fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                for (const ship of data.results) {
+                    starshipsArray.push(ship);
+                }
+                console.log('array', starshipsArray);
+                this.setState({ starships: starshipsArray });
+                console.log('state', this.state);
+            })
+    }))  
+  
+  }
+
+  render () {
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Star Wars Fact Finder</h1>
     </div>
-  );
+    );
+  } 
 }
 
 export default App;
