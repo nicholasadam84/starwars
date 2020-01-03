fd9c227e25792b2a658c449e27085f9f7eaec66e
nicholasadam84 <nicholasadam84@gmail.com>
2020-01-03 14:45:44 -0800

    Updates

---
+++ precache-manifest.5c13807ceac39bf973102cdf4e3f1832.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3a76b9f2df8cb47d15c009ab8048c38",
+    "url": "/starwars/index.html"
+  },
+  {
+    "revision": "762da92025cac1eb1501",
+    "url": "/starwars/static/css/2.299a5230.chunk.css"
+  },
+  {
+    "revision": "92902c1644d913eb7326",
+    "url": "/starwars/static/css/main.a1912adb.chunk.css"
+  },
+  {
+    "revision": "762da92025cac1eb1501",
+    "url": "/starwars/static/js/2.22b31529.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/starwars/static/js/2.22b31529.chunk.js.LICENSE"
+  },
+  {
+    "revision": "92902c1644d913eb7326",
+    "url": "/starwars/static/js/main.64045257.chunk.js"
+  },
+  {
+    "revision": "d3c35ed429a60fee06bf",
+    "url": "/starwars/static/js/runtime-main.ded91126.js"
+  }
+]);
